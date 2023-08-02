47198bf84fb7b15bd1de31052cae4d580c6a98c9
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-08-02 09:02:13 +0300

    Email already exists

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,18 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const Errors = require('../errors');
+
 const login = async (req, res) => {
   res.send('Login');
 };
 
 const register = async (req, res) => {
-  res.send('Register');
+  const { email } = req.body;
+  const emailAlreadyExist = await User.findOne({ email });
+  if (emailAlreadyExist)
+    throw new Errors.BadRequestError('Email already exists!');
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const logout = async (req, res) => {
